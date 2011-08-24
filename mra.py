ef54f66e4e5773b3660c972fccc10b58a51c89f2
Brandon Mechtley <bmechtley@gmail.com>
2011-08-23 17:44:48 -0700

    .

--- mra.py
+++ mra.py
@@ -1,88 +1,121 @@
 from pylab import *
 from scipy.stats import scoreatpercentile
 from random import choice
-
-#-----------------------------------------------------------------------------#
-# MRANode/MRATree:															  #
-#	These classes set up a tree for multiresolution analysis from dwt output. #
-#	This speeds up tree-based operations quite a bit, as no data needs to be  #
-#	copied and the trees can be modified in place.							  #
-#-----------------------------------------------------------------------------#
+import pywt
 
 class MRANode:
+	"""
+	MRA node object used by MRATree with links to ancestors and predecessors.
+	
+	Each MRA node represents a single coefficient of a multiresolution discrete
+	wavelet transform.
+	"""
+	
 	def __init__(self, value, parent, predecessor, side, level):
 		self.value = value
 		self.children = [None, None]
-		self.children2 = [None, None]
 		self.parent = parent
 		self.predecessor = predecessor
-		self.predecessor2 = None
-		self.parent2 = None
 		self.level = level
 		if self.parent is not None: self.parent.children[side] = self
 	
-	def childIndex(n):
+	def __str__(self):
+		return 'MRANode: %d, level %d' % (self.value, self.level)
+	
+	def childIndex(self):
+		"""Return the index of this node in its parent's list of children."""
+		
 		return self.parent.children.index(self)
 	
-	def __cmp__(self, other):
-		if other != None: return self.value - other.value
-		else: return 1
+	def ancestors(self):
+		"""Return a list of ancestors, going all the way to the root."""
+		
+		ancestor_list = []
+		n = self
+		
+		while n.parent:
+			ancestor_list.append(n.parent)
+			n = n.parent
+		
+		return ancestor_list
 	
-	def __str__(self):
-		return str(id(self))#'MRANode: %d, level %d' % (self.value, self.level)
+	def predecessors(self, k = 2):
+		"""Return a list of predecessors of length k."""
+		
+		predecessor_list = []
+		n = self
+		
+		for i in range(0, k):
+			predecessor_list.append(n.predecessor)
+			n = n.predecessor
+		
+		return predecessor_list
+	
+	def adopt(self, n1, n2):
+		"""Give up children and adopt two new ones (n1 and n2)."""
+		
+		self.children = [n1, n2]
+		n1.parent = self
+		n2.parent = self
 	
 class MRATree:
-	def __init__(self, input):
+	"""
+	Multiresolution analysis tree from constructed from a one-dimensional signal.
+	
+	An MRA tree is a binary tree where each node represents a DWT coefficient.
+	Using a tree structure speeds up tree-based operations, as no data needs to be 
+	copied and edges can be modified in-place.
+	"""
+	
+	def __init__(self, input, wavelet = 'db5', mode = 'per'):
 		self.root = None
 		self.nodes = []
-		self.data = input
-		
+		self.wavelet = wavelet
+		self.mode = mode
+		self.dwt = pywt.wavedec(input, wavelet, mode = mode, level = int(log2(len(input))) + 1)
 		parents = [None]
 		
-		for i in range(0, len(input)):
-			nodes = [
-				MRANode(input[i][j], parents[j / 2], input[i][j - 1], j % 2, i)
-				for j in range(0, len(input[i]))
-			]
+		for i in range(0, len(self.dwt)):
+			nodes = []
+			
+			for j in range(0, len(self.dwt[i])):
+				if j > 0:
+					nodes.append(MRANode(self.dwt[i][j], parents[j / 2], nodes[j - 1], j % 2, i))
+				else:
+					nodes.append(MRANode(self.dwt[i][j], parents[j / 2], None, j % 2, i))
+			
+			nodes[0].predecessor = nodes[-1]
 			parents = nodes
 			self.nodes.extend(nodes)
 			if i is 0: self.root = nodes[0]
 	
-	#-----------------------------------------------------------------------------#
-	# printLevels: print the tree in order of predecessors up to a certain level. #
-	#-----------------------------------------------------------------------------#
-	
-	def printLevels(self, maxlevel = -1, nodes = []):
-		if maxlevel < 0: maxlevel = max([n.level for n in self.nodes])
-		if not len(nodes): nodes = [self.root]
-		print 'Level %d:' % nodes[0].level, ', '.join([str(n) for n in nodes])
+	def reconstruct(self, node = None, level = 0, index = 0):
+		"""
+		Return a reconstructed one-dimensional signal from the MRA tree.
 		
-		if maxlevel <= nodes[0].level: return
-		else: self.printLevels(maxlevel, [n for n in list(flatten([n.children for n in nodes])) if n != None])
-	
-	#--------------------------------------------------------------#
-	# untree: convert tree to DWT format acceptable by PyWavelets. #
-	#--------------------------------------------------------------#
-	
-	def untree(self, node = None, level = 0, index = 0):
+		node, level, index -- used internally for recursive breadth-first reconstruction.
+		"""
+		
 		if node is None: node = self.root		
-		
-		self.data[level][index] = node.value
+		self.dwt[level][index] = node.value
 		
 		map(
 			lambda c:
-				self.untree(c, level + 1, index * 2 + c.parent.children.index(c))
+				self.reconstruct(c, level + 1, index * 2 + c.parent.children.index(c))
 				if c else None,
 			node.children
 		)
 		
-		if not level: return self.data
-	
-	#-----------------------------------------------#
-	# shake: randomly swap children for every node. #
-	#-----------------------------------------------#
+		if not level: return pywt.waverec(self.dwt, self.wavelet, mode = self.mode)
 	
 	def shake(self, node = None, level = 0, maxlevel = -1):
+		"""
+		Randomly swap children for every node.
+		
+		node, level -- used internally for recursive depth-first traversal.
+		maxlevel -- level at which to stop swapping children.
+		"""
+		
 		if maxlevel < 0 or level < maxlevel:
 			if not node: node = self.root
 			if len(node.children) and node.children[0] and node.children[1]:
@@ -90,39 +123,79 @@ class MRATree:
 			for child in [c for c in node.children if c != None]:
 				self.shake(child, level + 1, maxlevel)
 	
-	#----------------------------------------------#
-	# tap: wavelet tree learning as per TAPESTREA. #
-	#----------------------------------------------#
-	
-	def tap(self, p = 0.2):
-		threshold = scoreatpercentile([n.value for n in self.nodes], p)
+	def ascore(self, c, nancestors, threshold):
+		"""
+		Return wavelet tree learning ancestor similarity score.
 		
-		parents = []
-		nodes = [self.root]
+		c -- candidate node in question.
+		nancestors -- list of the original node's ancestors (MRANode instances).
+		threshold -- value under which two paths are considered similar. 
+		"""
 		
-		while len(nodes) > 0:
-			candidates = nodes[:]					# Nodes from which to choose randomly.
-			shuffle(candidates)
-			new_nodes = candidates[:]				# New ordering of nodes on this level.
+		cancestors = array([p.value for p in c.ancestors()])
+		ascores = abs(nancestors - cancestors)
+		ascores /= linspace(1, len(ascores), len(ascores))
+		return sum(cumsum(ascores) < threshold)
+		
+	def pscore(self, c, npredecessors, nk, threshold):
+		"""Return wavelet tree learning predecessor similarity score.
+		
+		c -- candidate node in question.
+		npredecessors -- list of the original node's predecessors.
+		nk -- number of predecessors to consider.
+		threshold -- value under which two paths are considered similar.
+		"""
+		
+		cpredecessors = array([p.value for p in c.predecessors(nk)])
+		pscores =  abs(npredecessors - cpredecessors)
+		return sum(cumsum(pscores) < threshold)
+		
+	def tap(self, p = 0.8, k = 0.2, maxlevel = -1):
+		"""
+		Rearrange tree using wavelet tree learning as per TAPESTREA.
+		
+		p -- what percentage of nodes to consider for the threshold. 
+			Larger values result in more variation.
+		k -- number of predecessors to consider in comparing node contexts.
+			Smaller values will tend to break up longer events.
+		maxlevel -- maximum level at which to rearrange nodes. Oftentimes
+			10 is sufficient. Use -1 (default) to rearrange all levels.
+			Note that children of rearranged nodes will be moved around
+			regardless.
+		"""
+		
+		threshold = scoreatpercentile([abs(n.value) for n in self.nodes], p) / 2.0
+		
+		parents = array([])
+		children = array([self.root])
+		
+		while len(children) > 0:
+			nodes = children.copy()
+			new_nodes = nodes.copy()
+			level = nodes[0].level
 			
-			if len(new_nodes) > 1:
-				# Create new ordering.
-				for i in range(0, len(new_nodes)):
-					new_nodes[i] = candidates.pop()
+			if level > 2 and (level < maxlevel or maxlevel < 0):				
+				nk = int(k * 2 ** level)
 				
-				# Re-assign predecessor relationships.
+				# Choose new node ordering.
+				for i in range(0, len(nodes)):
+					nancestors = array([p.value for p in nodes[i].ancestors()])
+					npredecessors = array([p.value for p in nodes[i].predecessors(nk)])
+					
+					ascores = array([self.ascore(c, nancestors, threshold) for c in nodes])
+					pscores = array([self.pscore(c, npredecessors, nk, threshold) for c in nodes])
+					
+					candidates = nodes[(ascores == max(ascores)) & (pscores == max(pscores))]
+					selection = choice(candidates)
+					new_nodes[i] = selection
+				
+				# Fix predecessor relationships.
 				for i in range(0, len(new_nodes)):
 					new_nodes[i].predecessor = new_nodes[i - 1]
 				
-				# Re-assign parent/child relationships.
-				node_stack = new_nodes[:]
-				
-				for p in parents:
-					for i in range(0, len(p.children)):
-						if len(node_stack):
-							p.children[i] = node_stack.pop()
-							p.children[i].parent = p
-							
-			parents = new_nodes[:]
-			nodes = [n for n in list(flatten([n.children for n in new_nodes])) if n != None]
-	
+				# Fix parent/child relationships.
+				for i in range(0, len(parents)):
+					parents[i].adopt(new_nodes[i * 2], new_nodes[i * 2 + 1])
+			
+			parents = nodes
+			children = array([c for c in array([n.children for n in nodes]).flat if c != None])
--- visualization.py
+++ visualization.py
@@ -1,8 +1,35 @@
 from pylab import *
 import pywt
 
-# Plot a scalogram in the current axes. Scaling turns on power of 2 height scaling per level.	
+def specs(textures, crop = True):
+	"""
+	Plot stacked spectrograms for each several sound textures.
+	
+	textures -- a list of Texture instances.
+	crop -- toggle cropping spectrograms to the minimum duration amongst the textures.
+	"""
+	
+	minduration = min([len(t.samples) / t.fs for t in textures])
+	
+	for i in range(0, len(textures)):
+		subplot(len(textures), 1, i)
+		samples = textures[i].samples[0:minduration * textures[i].fs] if crop else textures[i].samples
+		specgram(samples, Fs = textures[i].fs)
+		title(textures[i].file.split('/')[-1])
+		if i is 0: xlabel('Time (s)')
+		else: xticks([])
+		ylabel('Frequency (Hz)')
+		
+	show()
+
 def scalogram(tree, scaling = True, levels = None):	
+	"""
+	Plot a scalogram for PyWavelets DWT output.
+	
+	scaling -- toggles on power-of-2 height scaling per level.
+	levels -- maximum number of levels to plot.
+	"""
+	
 	bottom = 0
 	if not levels: levels = len(tree)
 	
@@ -25,6 +52,16 @@ def scalogram(tree, scaling = True, levels = None):
 
 # Wavelet packet decomposition scalogram.
 def packet_scalogram(signal, wavelet = pywt.Wavelet('db5'), mode = 'ppd', order = 'freq', fs = 44100):
+	"""
+	Plot a wavelet packet decomposition (WPD) scalogram.
+	
+	signal -- the one-dimensional signal to use for the scalogram.
+	wavelet -- the wavelet to use. This is an element of pywt.wavelist()
+	mode -- the wavelet extension mode to use for boundaries.
+	order -- how to order the nodes in the scalogram.
+	fs -- samplerate of the signal for plotting axes.
+	"""
+	
 	levels = pywt.dwt_max_level(len(signal), wavelet.dec_len) / 2
 	
 	wp = pywt.WaveletPacket(signal, wavelet, mode, maxlevel = levels)
